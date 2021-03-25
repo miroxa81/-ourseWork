7e0c3f82d63a5b5fccb62a7bd44c8836b720ede8
miroxa81 <miroxa81@hotmail.com>
2021-03-26 00:35:52 +0200

    start

---
+++ FileManager/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+	class Data
+	{
+		private DirectoryInfo _currentDirectory;
+		private DirectoryInfo _directories;
+		private DriveInfo _drive;
+
+		public DirectoryInfo Directories
+		{
+			get { return _directories; }
+			private set { _currentDirectory.GetDirectories(); }
+		}
+
+		public DirectoryInfo CurrentDirectory
+		{
+			get { return _currentDirectory; }
+			set { _currentDirectory = value; }
+		}
+
+
+	}
+}
---
+++ FileManager/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileManager
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			DriveInfo startDriveLeft = GetDrives()[0];
+			DriveInfo startDriveRight = GetDrives()[1];
+			var rootLeft = GetRootDir(startDriveLeft);
+			var rootRight = GetRootDir(startDriveRight);
+			var leftSide = GetDirInfo(rootLeft);
+			var rigthSide = GetDirInfo(rootRight);
+			Console.ReadKey();
+
+			var sideData = new Data();
+
+			//sideData.CurrentDirectory = startDriveLeft
+		}
+
+
+
+
+		static DriveInfo[] GetDrives()
+		{
+			DriveInfo[] drives = DriveInfo.GetDrives();
+			return drives;
+		}
+
+		static DirectoryInfo GetRootDir(DriveInfo Drive)
+		{
+			return Drive.RootDirectory;
+		}
+		static (DirectoryInfo[] dirs, FileInfo[] files) GetDirInfo(DirectoryInfo currentDirectory)
+		{
+			return (currentDirectory.GetDirectories(), currentDirectory.GetFiles());
+		}
+		static void WriteColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.Write(txt);
+			Console.ResetColor();
+		}
+	}
+
+
+
+}
